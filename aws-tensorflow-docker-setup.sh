da3219d6a2f29fd13892ccce8141a5e912a1eade
Max Melnick <maxmelnick@gmail.com>
2016-06-10 17:36:35 -0400

    added comments

--- aws-tensorflow-docker-setup.sh
+++ aws-tensorflow-docker-setup.sh
@@ -4,18 +4,20 @@ DRIVER_VERSION='361.45.11'
 PRODUCT_NAME='K520'
 
 
-# 0) Prepare nVidia Driver
+# Prepare nVidia Driver
 
 if [ -d ~/tmp ]; then
     rm -rf ~/tmp
 fi
 
-## a) Install requested packages
+
+## Install basic packages
 sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev git libreadline-dev \
     libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev linux-image-extra-`uname -r`
 
-## b) Check whether the host has GPU Device
+
+## Check whether the host has GPU Device
 gpu_count = $(lspci |grep -i VGA|grep -i -c nVidia)
 
 if [ $gpu_count == 0 ]; then
@@ -23,7 +25,8 @@ if [ $gpu_count == 0 ]; then
     exit 1
 fi
 
-## c) Install nVidia Driver
+
+## Install nVidia Driver
 mkdir ~/tmp
 cd ~/tmp
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
@@ -39,9 +42,13 @@ if [ $(nvidia-smi -q |grep 'Product\ Name' |grep -c $PRODUCT_NAME) != 1 ]; then
     exit 3
 fi
 
+
+# Install docker
+# NOTE: you'll have to exit out of ubuntu user and log back in again in order to be able to use `docker` command without sudo
 curl -fsSL https://get.docker.com/ | sh
 
 sudo usermod -aG docker ubuntu
+
 
 # Install nvidia-docker and nvidia-docker-plugin
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.2/nvidia-docker_1.0.0.rc.2-1_amd64.deb
