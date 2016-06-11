2b6363689868c3af245cf38f852b32b87b5ca72d
Max Melnick <maxmelnick@gmail.com>
2016-06-10 22:40:24 -0400

    added tensorflow install to script

--- anaconda_install.sh
+++ anaconda_install.sh
@@ -40,3 +40,39 @@ echo $conda_path
 cat >> ~/.bashrc << END
 PATH=:$conda_path:\$PATH
 END
+
+
+# Install Java 8 (required for Bazel)
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -y update
+sudo apt-get install -y oracle-java8-installer
+
+
+# Install Bazel
+echo "deb http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+sudo apt-get -y update && sudo apt-get install -y bazel
+sudo apt-get upgrade -y bazel
+
+
+# Install other Tensorflow dependencies
+sudo apt-get install -y python-numpy swig python-dev python-wheel
+
+
+# Install Tensorflow from source
+cd /
+
+git clone --recursive https://github.com/tensorflow/tensorflow
+
+cd /tensorflow/tensorflow
+
+export TF_CUDA_COMPUTE_CAPABILITIES="3.0"
+
+./configure && \
+    bazel build -c opt --config=cuda tensorflow/tools/pip_package:build_pip_package && \
+    bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip && \
+    pip install --upgrade /tmp/pip/tensorflow-*.whl
+
+# Build image retrainer
+# see: https://www.tensorflow.org/versions/master/how_tos/image_retraining/index.html
+RUN bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
