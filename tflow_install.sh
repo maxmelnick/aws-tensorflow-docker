5d50281a67699ecbf9df4181ffc9fc8f96b0dac3
Max Melnick <maxmelnick@gmail.com>
2016-06-13 16:36:02 -0400

    changes to tensorflow install

--- tflow_install.sh
+++ tflow_install.sh
@@ -12,6 +12,33 @@ fi
 # install git
 apt_packages=(
     git
+    build-essential
+	python-pip
+	libfreetype6-dev
+	libxft-dev
+	libncurses-dev
+	libopenblas-dev
+	gfortran
+	python-matplotlib
+	libblas-dev
+	liblapack-dev
+	libatlas-base-dev
+	python-dev
+	python-pydot
+	linux-headers-generic
+	linux-image-extra-virtual
+	unzip
+	python-numpy
+	swig
+	python-pandas
+	python-sklearn
+	unzip
+	wget
+	pkg-config
+	zip
+	g++
+	zlib1g-dev
+	python-wheel
 )
 
 sudo apt-get -y update
@@ -19,32 +46,28 @@ sudo apt-get -y upgrade
 
 echo "Installing apt-get packages..."
 sudo apt-get install -y ${apt_packages[@]}
-
-sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev
 sudo pip install -U -y pip
-
 sudo apt-get clean
 
 #install anaconda
-# anaconda=Anaconda2-4.0.0-Linux-x86_64.sh
-# cd ~
-# if [ ! -f $anaconda ]
-# 	then
-# 		echo "No Anaconda install file pre-loaded. Downloading Anaconda"
-# 		echo "NOTE: downloading and installing Anaconda2-4.0.0-Linux-x86_64.sh"
-#     	wget --quiet http://repo.continuum.io/archive/$anaconda
-# fi
-# chmod +x $anaconda
+anaconda=Anaconda2-4.0.0-Linux-x86_64.sh
+cd ~
+if [ ! -f $anaconda ]
+	then
+		echo "No Anaconda install file pre-loaded. Downloading Anaconda"
+		echo "NOTE: downloading and installing Anaconda2-4.0.0-Linux-x86_64.sh"
+    	wget --quiet http://repo.continuum.io/archive/$anaconda
+fi
+chmod +x $anaconda
 
-# sudo ./$anaconda -b -p /opt/anaconda
+sudo ./$anaconda -b -p /opt/anaconda
 
-# conda_path=/opt/anaconda/bin
-# echo $conda_path
+conda_path=/opt/anaconda/bin
+echo $conda_path
 
-# cat >> ~/.bashrc << END
-# PATH=:$conda_path:\$PATH
-# END
-
+cat >> ~/.bashrc << END
+PATH=:$conda_path:\$PATH
+END
 
 
 
@@ -61,25 +84,21 @@ sudo apt-get -y update && sudo apt-get install -y bazel
 sudo apt-get upgrade -y bazel
 
 
-# Install other Tensorflow dependencies
-sudo apt-get install -y python-numpy swig python-dev python-wheel
-
-
 # Install Tensorflow from source
 cd ~
 
 git clone --recursive https://github.com/tensorflow/tensorflow
 
-# cd tensorflow
+cd tensorflow
 
-# export TF_CUDA_COMPUTE_CAPABILITIES="3.0"
+export TF_CUDA_COMPUTE_CAPABILITIES="3.0"
 
-# ./configure
-# bazel build -c opt --config=cuda //tensorflow/cc:tutorials_example_trainer
-# bazel build -c opt --config=cuda tensorflow/tools/pip_package:build_pip_package
-# bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip
-# pip install --upgrade /tmp/pip/tensorflow-*.whl
+./configure
+bazel build -c opt --config=cuda //tensorflow/cc:tutorials_example_trainer
+bazel build -c opt --config=cuda tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip
+pip install --upgrade /tmp/pip/tensorflow-0.8.0-py2-none-any.whl
 
-# # Build image retrainer
-# # see: https://www.tensorflow.org/versions/master/how_tos/image_retraining/index.html
-# bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
+# Build image retrainer
+# see: https://www.tensorflow.org/versions/master/how_tos/image_retraining/index.html
+bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
