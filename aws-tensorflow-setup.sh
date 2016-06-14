b8c6a4d96a0297f79ba50c43068024dcc453ff3b
Max Melnick <maxmelnick@gmail.com>
2016-06-14 07:41:44 -0400

    install script updates

--- aws-tensorflow-setup.sh
+++ aws-tensorflow-setup.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+start_seconds="$(date +%s)"
+echo "Welcome to the initialization script."
+
+ping_result="$(ping -c 2 8.8.4.4 2>&1)"
+if [[ $ping_result != *bytes?from* ]]; then
+    echo "Network connection unavailable. Try again later."
+    exit 1
+fi
+
+apt_packages=(
+    git
+    build-essential
+    python-dev
+    python-numpy
+    swig
+    python-wheel
+    zip
+    zlib1g-dev
+)
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+echo "Installing apt-get packages..."
+sudo apt-get install -y ${apt_packages[@]}
+sudo apt-get clean
+
 DRIVER_VERSION='361.45.11'
 PRODUCT_NAME='K520'
 
@@ -43,6 +70,7 @@ if [ $(nvidia-smi -q |grep 'Product\ Name' |grep -c $PRODUCT_NAME) != 1 ]; then
 fi
 
 
+
 ## d) Install CUDA-toolkit 7.5
 cd ~/tmp
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
@@ -56,18 +84,18 @@ else
 fi
 cd ~
 
+sudo apt-get install -y awscli
+
 ## e) Install CuDNN v4
-if [ -f ~/cudnn-7.0-linux-x64-v4.0-prod.tgz ]; then
-    cp ~/cudnn-7.0-linux-x64-v4.0-prod.tgz ~/tmp
-    cd ~/tmp
-    tar xvzf cudnn-7.0-linux-x64-v4.0-prod.tgz
-    sudo cp cuda/include/cudnn.h /usr/local/cuda/include
-    sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-    cd ~
-else
-    echo 'Error: Can not found CuDNN.'
-    exit 5
-fi
+cd ~
+aws s3 cp s3://mjm-image-classifier/cudnn-7.0-linux-x64-v4.0-prod.tgz . --region us-east-1
+cp ~/cudnn-7.0-linux-x64-v4.0-prod.tgz ~/tmp
+cd ~/tmp
+tar xvzf cudnn-7.0-linux-x64-v4.0-prod.tgz
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+cd ~
+
 
 ## Clean up
 cd ~
@@ -76,3 +104,41 @@ rm -rf ./tmp
 ## f) Finish configure
 echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"' >> ~/.bashrc
 echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+
+
+#install anaconda
+anaconda=Anaconda2-4.0.0-Linux-x86_64.sh
+cd ~
+if [ ! -f $anaconda ]
+    then
+        echo "No Anaconda install file pre-loaded. Downloading Anaconda"
+        echo "NOTE: downloading and installing Anaconda2-4.0.0-Linux-x86_64.sh"
+        wget --quiet http://repo.continuum.io/archive/$anaconda
+fi
+chmod +x $anaconda
+
+sudo ./$anaconda -b -p /opt/anaconda
+
+conda_path=/opt/anaconda/bin
+echo $conda_path
+
+cat >> ~/.bashrc << END
+PATH=:$conda_path:\$PATH
+END
+
+source ~/.bashrc
+
+conda install pip -y
+
+
+# Install Java 8 (required for Bazel)
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get -y update
+sudo apt-get install -y oracle-java8-installer
+
+
+# Install Bazel
+echo "deb http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+sudo apt-get -y update && sudo apt-get install -y bazel
+sudo apt-get upgrade -y bazel
--- tflow_install.sh
+++ tflow_install.sh
@@ -1,102 +1,24 @@
 #!/usr/bin/env bash
 
-start_seconds="$(date +%s)"
-echo "Welcome to the initialization script."
-
-ping_result="$(ping -c 2 8.8.4.4 2>&1)"
-if [[ $ping_result != *bytes?from* ]]; then
-    echo "Network connection unavailable. Try again later."
-    exit 1
-fi
-
-# install git
-apt_packages=(
-    git
-    build-essential
-	python-pip
-	libfreetype6-dev
-	libxft-dev
-	libncurses-dev
-	libopenblas-dev
-	gfortran
-	python-matplotlib
-	libblas-dev
-	liblapack-dev
-	libatlas-base-dev
-	python-dev
-	python-pydot
-	linux-headers-generic
-	linux-image-extra-virtual
-	python-numpy
-	swig
-	python-pandas
-	python-sklearn
-	unzip
-	wget
-	pkg-config
-	zip
-	g++
-	zlib1g-dev
-	python-wheel
-)
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
-echo "Installing apt-get packages..."
-sudo apt-get install -y ${apt_packages[@]}
-sudo pip install -U -y pip
-sudo apt-get clean
-
-#install anaconda
-anaconda=Anaconda2-4.0.0-Linux-x86_64.sh
-cd ~
-if [ ! -f $anaconda ]
-	then
-		echo "No Anaconda install file pre-loaded. Downloading Anaconda"
-		echo "NOTE: downloading and installing Anaconda2-4.0.0-Linux-x86_64.sh"
-    	wget --quiet http://repo.continuum.io/archive/$anaconda
-fi
-chmod +x $anaconda
-
-sudo ./$anaconda -b -p /opt/anaconda
-
-conda_path=/opt/anaconda/bin
-echo $conda_path
-
-cat >> ~/.bashrc << END
-PATH=:$conda_path:\$PATH
-END
 
 
 
-# Install Java 8 (required for Bazel)
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get -y update
-sudo apt-get install -y oracle-java8-installer
-
-
-# Install Bazel
-echo "deb http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
-sudo apt-get -y update && sudo apt-get install -y bazel
-sudo apt-get upgrade -y bazel
 
 
 # Install Tensorflow from source
-cd ~
+# cd ~
 
-git clone --recursive https://github.com/tensorflow/tensorflow
+# git clone --recursive https://github.com/tensorflow/tensorflow
 
-cd tensorflow
+# cd tensorflow
 
-export TF_CUDA_COMPUTE_CAPABILITIES="3.0"
+# export TF_CUDA_COMPUTE_CAPABILITIES="3.0"
 
-./configure
-bazel build -c opt --config=cuda tensorflow/tools/pip_package:build_pip_package
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip
-pip install --upgrade /tmp/pip/tensorflow-0.8.0-py2-none-any.whl
+# ./configure && \
+# 	bazel build -c opt --config=cuda tensorflow/tools/pip_package:build_pip_package && \
+# 	bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip && \
+# 	pip install --upgrade /tmp/pip/tensorflow-0.8.0-py2-none-any.whl
 
-# Build image retrainer
-# see: https://www.tensorflow.org/versions/master/how_tos/image_retraining/index.html
-bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
+# # Build image retrainer
+# # see: https://www.tensorflow.org/versions/master/how_tos/image_retraining/index.html
+# bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
